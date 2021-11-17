bff00186c289741590ada619083a17c0c1906caf
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2021-11-17 14:48:38 -0300

    Step 2

--- error-reporting-cpp/src/error_message.h
+++ error-reporting-cpp/src/error_message.h
@@ -4,28 +4,28 @@
 
 namespace error_reporting {
 
-    struct error_message_declaration {
-        explicit error_message_declaration(
+    struct error_message_declaration_internal {
+        explicit error_message_declaration_internal(
                 const char* _error_code,
                 const char* _error_message,
                 std::initializer_list<const char*> _error_mitigations,
-                std::initializer_list<const char*> _parameters);
+                size_t _nr_parameters,
+                const char* const* _parameters);
         const char* error_code;
         const char* error_message;
         const size_t nr_error_mitigations;
         const char* const * error_mitigations;
         const size_t nr_parameters;
-        const char* const * parameters;
-        error_message_declaration * next;
-
+        const char * const * parameters;
+        error_message_declaration_internal * next;
     };
 
     class error_message_declaration_container {
     public:
         error_message_declaration_container() : root(nullptr) {}
 
-        void register_error_message(error_message_declaration * msg) {
-            error_message_declaration * n = root;
+        void register_error_message(error_message_declaration_internal * msg) {
+            error_message_declaration_internal * n = root;
 
             if (nullptr == root) {
                 root = msg;
@@ -39,7 +39,7 @@ namespace error_reporting {
 
         std::string collect_error_messages_as_json() {
             std::ostringstream msg_stream;
-            error_message_declaration * n = root;
+            error_message_declaration_internal * n = root;
             msg_stream << "[";
             while(n != nullptr) {
                 msg_stream << R"({ "code": ")" << n->error_code << R"(" ,  "message": ")" << n->error_message
@@ -47,7 +47,7 @@ namespace error_reporting {
                 for (size_t idxParam(0); idxParam < n->nr_parameters; idxParam++) {
                     msg_stream << "\"" <<n->parameters[idxParam] << "\",";
                 }
-                msg_stream << "], \"mitigations\":[";
+                msg_stream << "], \"mitigations\" : [";
                 for (size_t idxMitigation(0); idxMitigation < n->nr_error_mitigations; idxMitigation++) {
                     msg_stream << "\"" << n->error_mitigations[idxMitigation] << "\",";
                 }
@@ -59,7 +59,7 @@ namespace error_reporting {
         }
 
     private:
-        error_message_declaration * root;
+        error_message_declaration_internal * root;
     };
 
     extern error_message_declaration_container g_error_message_container;
@@ -68,109 +68,89 @@ namespace error_reporting {
 #endif
 
 
-    inline error_message_declaration::error_message_declaration(
+    inline error_message_declaration_internal::error_message_declaration_internal(
             const char* _error_code,
             const char* _error_message,
             std::initializer_list<const char*> _error_mitigations,
-            std::initializer_list<const char*> _parameters) :
+            size_t _nr_parameters,
+            const char* const * _parameters) :
             error_code(_error_code),
             error_message(_error_message),
             nr_error_mitigations(_error_mitigations.size()),
             error_mitigations(_error_mitigations.begin()),
-            nr_parameters(_parameters.size()),
-            parameters(_parameters.begin()) {
+            nr_parameters(_nr_parameters),
+            parameters(_parameters) {
         g_error_message_container.register_error_message(this);
     }
 
-#define DECLARE_PARAMETER_1(name, p1) const std::array<const char*, 1> g_parameter_##name = { p1 };
-#define DECLARE_PARAMETER_2(name, p1, p2) const std::array<const char*, 2> g_parameter_##name = { p1, p2 };
-#define DECLARE_PARAMETER_3(name, p1, p2, p3) const std::array<const char*, 3> g_parameter_##name = { p1, p2, p3 };
-#define DECLARE_PARAMETER_4(name, p1, p2, p3, p4) const std::array<const char*, 4> g_parameter_##name = { p1, p2, p3, p4 };
-#define DECLARE_PARAMETER_5(name, p1, p2, p3, p4, p5) const std::array<const char*, 5> g_parameter_##name = { p1, p2, p3, p4, p5 };
-#define DECLARE_PARAMETER_6(name, p1, p2, p3, p4, p5, p6) const std::array<const char*, 6> g_parameter_##name = { p1, p2, p3, p4, p5, p6 };
-#define DECLARE_PARAMETER_7(name, p1, p2, p3, p4, p5, p6, p7) const std::array<const char*, 7> g_parameter_##name = { p1, p2, p3, p4, p5, p6, p7 };
-#define DECLARE_PARAMETER_8(name, p1, p2, p3, p4, p5, p6, p7, p8) const std::array<const char*, 8> g_parameter_##name = { p1, p2, p3, p4, p5, p6, p7, p8 };
-#define DECLARE_PARAMETER_9(name, p1, p2, p3, p4, p5, p6, p7, p8, p9) const std::array<const char*, 9> g_parameter_##name = { p1, p2, p3, p4, p5, p6, p7, p8, p9 };
-#define DECLARE_PARAMETER_10(name, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10) const std::array<const char*, 10> g_parameter_##name = { p1, p2, p3, p4, p5, p6, p7, p8, p9, p10 };
-
-#   define GET_ARG_COUNT(...) INTERNAL_GET_ARG_COUNT_PRIVATE(0, ## __VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
-#   define INTERNAL_GET_ARG_COUNT_PRIVATE(_0, _1_, _2_, _3_, _4_, _5_, _6_, _7_, _8_, _9_, _10_, count, ...) count
-
-#define ERR_ARRAY(...) { __VA_ARGS__ }
-#define DECLARE_ERROR_CODE(error_code, error_message, mitigations, parameters) \
-error_message_declaration g_##error_code { \
-    #error_code, \
-    error_message, \
-    mitigations, \
-    parameters \
+    struct error_message_declaration : error_message_declaration_internal {
+        explicit error_message_declaration(
+                const char* _error_code,
+                const char* _error_message,
+                std::initializer_list<const char*> _error_mitigations) :
+                error_message_declaration_internal( _error_code, _error_message, _error_mitigations,
+                                                    0, nullptr){}
+        std::string str() {
+            std::ostringstream os;
+            os << "[" << error_code << "] - " << error_message;
+            return os.str();
+        }
     };
 
-//    template<int flags = 0>
-//    class ErrorBuilder {
-//
-//        template<int anyflags>
-//        friend class NodeBuilder;
-//        enum Flags {
-//            Description,
-//            Name,
-//            Value,
-//            TotalFlags
-//        };
-//
-//    public:
-//        template<int anyflags>
-//        ErrorBuilder(const ErrorBuilder<anyflags>& cpy) : m_buildingNode(cpy.m_buildingNode) {};
-//
-//        template<int pos>
-//        using NextBuilder = ErrorBuilder<flags | (1 << pos)>;
-//
-//        //The && at the end is import so you can't do b.withDescription() where b is a lvalue.
-//        NextBuilder<Description> withDescription( const char* desc ) && {
-//            m_buildingNode.description = desc;
-//            return *this;
-//        }
-//        //other with* functions etc...
-//
-//        //needed so that if you store an incomplete builder in a variable,
-//        //you can easily create a copy of it. This isn't really a problem
-//        //unless you have optional values
-//        ErrorBuilder<flags> operator()() & {
-//            return ErrorBuilder<flags>(*this);
-//        }
-//
-//        //Implicit cast from node builder to node, but only when building is complete
-//        explicit operator typename std::conditional<flags == (1 << TotalFlags) - 1, error_message, void>::type() {
-//            return m_buildingNode;
-//        }
-//    private:
-//        error_message m_buildingNode;
-//    };
-//
-//    template<typename ...Args>
-//    struct parameters {
-//    };
-//
-//    //template<typename ...Params>
-//    struct error_message_description {
-//        const char *code;
-//        const char *message;
-//      //  typedef parameters<Params...> tParameters;
-//    };
+    template<int COUNTER>
+    class ErrorBuilder {
+    public:
+        explicit ErrorBuilder(const error_message_declaration_internal& _src) : src(_src) {
+            m_builder << "[" << src.error_code << "] - " << src.error_message << ": (";
+        };
 
+        explicit ErrorBuilder(const error_message_declaration_internal& _src, const std::string && str)
+        : src(_src) {
+            m_builder << str;
+        };
 
+        template<typename T>
+        ErrorBuilder<COUNTER - 1> param( const T & param ) && {
+            static_assert(COUNTER > 0);
+            m_builder << param;
+            if (COUNTER > 1) {
+                m_builder << ",";
+            }
+            return ErrorBuilder<COUNTER - 1>(src, m_builder.str());
+        }
 
-//    class error_builder {
-//    public:
-//        error_builder(const char *code, const char *message) : code(code) {}
-//
-//    private:
-//        const char *code;
-//        const char *message;
-//        std::ostringstream msg_stream;
-//    };
+        //Implicit cast from error builder to string, but only when building is complete
+        typename std::conditional<0 == COUNTER, std::string, void>::type str() {
+            m_builder << ")";
+            return m_builder.str();
+        }
 
+    private:
+        std::ostringstream m_builder;
+        const error_message_declaration_internal& src;
+    };
 
+    template<size_t N_PARAM>
+    struct error_message_declaration_with_param : error_message_declaration_internal {
+        explicit error_message_declaration_with_param(
+                const char* _error_code,
+                const char* _error_message,
+                std::initializer_list<const char*> _error_mitigations,
+                std::initializer_list<const char*> _parameters) :
+                error_message_declaration_internal( _error_code, _error_message, _error_mitigations,
+                                                    _parameters.size(), _parameters.begin()){}
 
+        ErrorBuilder<N_PARAM> build() {
+            return ErrorBuilder<N_PARAM>(*this);
+        }
+    };
 }
 
+#define DECLARE_ERR_MSG(EC, EM, MITIGATIONS) error_message_declaration g_##EC(#EC, EM, MITIGATIONS)
+#define DECLARE_ERR_MSG_PARAMS(EC, EM, MITIGATIONS, PARAMS) \
+                                                            \
+error_message_declaration_with_param<> g_##EC(#EC, EM, MITIGATIONS, PARAMS)
+
+
 #endif //ERROR_REPORTING_CPP_ERROR_MESSAGE_H
+
--- error-reporting-cpp/test/test.cpp
+++ error-reporting-cpp/test/test.cpp
@@ -6,18 +6,24 @@
 
 
 using namespace error_reporting;
-
-const std::array<const char*, 2> g_out_of_memory_parameters = {
-        "dummy1", "dummy2"
-};
-
-const std::array<const char*, 2> g_out_of_memory_error_mitigations = {
-        "dummy1", "dummy2"
-};
-
-DECLARE_ERROR_CODE(EC_DB_1, "Out of memory", ERR_ARRAY("dummy1", "dummy2"), ERR_ARRAY("Buy more RAM"))
+#define TEST_(...) { __VA_ARGS__ }
+DECLARE_ERR_MSG(EC_DB_1, "Out of memory", {"Buy more RAM"});
+DECLARE_ERR_MSG_PARAMS(EC_DB_2, "hd error", {"Buy new disk"},TEST_("Dummy1", "Dummy2"));
+//error_message_declaration g_db_1_error("EC_DB_1", "Out of memory", {"Buy more RAM"});
+//error_message_declaration_with_param<2> g_db_2_error("EC_DB_2", "hd error",
+//                                                     {"Buy new disk"}, {"Dummy1", "Dummy2"});
 
 TEST(ErrorMessage, BasicErrorMessage){
-    const std:: string res = g_error_message_container.collect_error_messages_as_json();
-    EXPECT_EQ("[{ \"code\": \"EC_DB_1\" ,  \"message\": \"Out of memory\", \"parameters\" : [\"Buy more RAM\",], \"mitigations\":[\"dummy1\",\"dummy2\",]},]", res);
+    const std::string res = g_error_message_container.collect_error_messages_as_json();
+    EXPECT_EQ("[{ \"code\": \"EC_DB_1\" ,  \"message\": \"Out of memory\", \"parameters\" : [], \"mitigations\" : [\"Buy more RAM\",]},{ \"code\": \"EC_DB_2\" ,  \"message\": \"hd error\", \"parameters\" : [\"Dummy1\",\"Dummy2\",], \"mitigations\" : [\"Buy new disk\",]},]", res);
+}
+
+TEST(ErrorMessage, BuildSimpleErrorMessage){
+    const std::string res = g_EC_DB_1.str();
+    EXPECT_EQ("[EC_DB_1] - Out of memory", res);
+}
+
+TEST(ErrorMessage, BuildParametrizedErrorMessage){
+    const std::string res = g_db_2_error.build().param(1).param("blabla").str();
+    EXPECT_EQ("[EC_DB_2] - hd error: (1,blabla)", res);
 }
