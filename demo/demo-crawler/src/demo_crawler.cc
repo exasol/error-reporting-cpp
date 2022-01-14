7356403c1bdb259ec40b36750a64fc5882420f8d
Thomas Ubensee <34603111+tomuben@users.noreply.github.com>
2022-01-14 16:20:27 -0300

    Fix crawler of demo project.

--- demo/demo-crawler/src/demo_crawler.cc
+++ demo/demo-crawler/src/demo_crawler.cc
@@ -1,12 +1,7 @@
-#define ERROR_MESSAGE_COLLECTION
-#include "demo/demo-lib/src/demo_lib.h"
-#include "demo/demo-lib/src/demo_lib_errors.h"
 #include "error-reporting-cpp/src/error_message.h"
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    demo_errors::DEMO_1.str();
-    low_value_div(1,1);
     std::cout << ::error_reporting::g_error_message_container.collect_error_messages_as_json();
     return 0;
 }
